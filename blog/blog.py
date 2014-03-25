bb145d0f444e95a1075b9a1911c2f30ccf716f5b
Chia-Chen Kuo <gogoairen@gmail.com>
2014-03-25 15:11:23 -0700

    signup page

--- blog/blog.py
+++ blog/blog.py
@@ -16,6 +16,7 @@
 #
 import os
 import webapp2
+import re
 
 # Jinja2 is a powerful templating engine modeled after Django's templating system. 
 # The idea is to separate your logic from your presentation, and make your code clean and well-defined in the process.
@@ -53,6 +54,61 @@ class Rot13(BaseHandler):
             rot13 = text.encode('rot13')
         self.render('rot13-form.html', text = rot13)
 
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return USER_RE.match(username)
+
+PWD_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return PWD_RE.match(password)
+
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
+def valid_email(email):
+    return EMAIL_RE.match(email)
+
+class Signup(BaseHandler):
+    def get(self):
+        self.render('signup-form.html')
+
+    def post(self):
+        username = self.request.get("username")
+        password = self.request.get("password")
+        verify = self.request.get("verify")
+        email = self.request.get("email")
+
+        error_username = ""
+        error_pwd = ""
+        error_verify = ""
+        error_email = ""
+        has_error = False
+
+        if not valid_username(username):
+            error_username = "That's not a valid username."
+            has_error = True
+        if not valid_password(password):
+            error_pwd = "That wasn't a valid password."
+            has_error = True
+        if password != verify:
+            error_verify = "Your passwords didn't match."
+            has_error = True
+        if email != "" and not valid_email(email):
+            error_email = "That's not a valid email."
+            has_error = True
+        
+        if has_error:
+            self.render('signup-form.html', username=username, email=email, 
+                error_username=error_username, error_pwd=error_pwd, error_verify=error_verify, error_email=error_email)
+        else:
+            self.redirect('/blog/welcome?username=' + username)
+        #self.render('signup-form.html')
+
+class Welcome(BaseHandler):
+    def get(self):
+        username = self.request.get("username")
+        self.render('welcome.html', username=username)
+
 app = webapp2.WSGIApplication([
-    ('/blog/rot13', Rot13)
+    ('/blog/rot13', Rot13), 
+    ('/blog/signup', Signup),
+    ('/blog/welcome', Welcome)
 ], debug=True)
